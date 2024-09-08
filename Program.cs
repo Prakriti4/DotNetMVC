a11348c5c23cd5d3ec0ca9daab5f595f9c4ae2f4
Prakriti4 <114340135+Prakriti4@users.noreply.github.com>
2024-09-08 13:25:14 +0545

    Necessary services are kept here

--- Program.cs
+++ Program.cs
@@ -1,18 +1,27 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 using ViewModelTableFormation.Data;
+using ViewModelTableFormation.Helpers;
+using ViewModelTableFormation.UnitOfWork;
+using ViewModelTableFormation.Services;
+using ViewModelTableFormation.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
 builder.Services.AddControllersWithViews();
 
+
 // Get the connection string
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 // Configure DbContext with SQL Server
 builder.Services.AddDbContext<SchoolIdentityDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseSqlServer(connectionString, sqlOptions =>
+        sqlOptions.EnableRetryOnFailure()
+    )
+);
+
 
 // Configure Identity with the same DbContext
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
@@ -24,10 +33,18 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
 builder.Services.AddAuthorization(options =>
 {
 	options.AddPolicy("RequireAdministratorRole",
-		 policy => policy.RequireRole("Administrator"));
-	options.AddPolicy("RequireUserRole",
-		 policy => policy.RequireRole("User"));
+         policy => policy.RequireRole("Administrator"));
+options.AddPolicy("RequireUserRole",
+     policy => policy.RequireRole("User"));
 });
+
+//Adding automapper in the solution 
+builder.Services.AddAutoMapper(typeof(AutoMapperProfile).Assembly);
+builder.Services.AddScoped<IUnitOfWork, StudentUOW>();
+builder.Services.AddScoped<IStudentService, StudentService>();
+builder.Services.AddScoped<IStudentRepository, StudentRepository>();
+
+
 
 var app = builder.Build();
 
@@ -48,10 +65,12 @@ app.UseRouting();
 app.UseAuthentication(); // Enable authentication
 app.UseAuthorization();  // Enable authorization
 
+app.MapRazorPages();
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.MapRazorPages();
+
 
 app.Run();
