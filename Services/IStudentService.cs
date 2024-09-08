43866e45a8d4c19faa64c322a226630a84048744
Prakriti4 <114340135+Prakriti4@users.noreply.github.com>
2024-09-08 13:22:56 +0545

    Services Folder added

---
+++ Services/IStudentService.cs
@@ -0,0 +1,16 @@
+﻿using ViewModelTableFormation.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ViewModelTableFormation.Services
+{
+    public interface IStudentService
+    {
+        Task<IEnumerable<Student>> GetAllStudentAsync();
+        Task<Student> GetStudentByIdAsync(int id);
+        Task AddStudentAsync(Student student);
+        Task UpdateStudentAsync(Student student);
+        Task DeleteStudentAsync(int id);
+        Task<IEnumerable<Course>> GetAllCoursesAsync();
+    }
+}
---
+++ Services/StudentService.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ViewModelTableFormation.Data;
+using ViewModelTableFormation.Models;
+using ViewModelTableFormation.UnitOfWork;
+
+namespace ViewModelTableFormation.Services
+{
+    public class StudentService : IStudentService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly SchoolIdentityDbContext _context;
+
+        public StudentService(IUnitOfWork unitOfWork, SchoolIdentityDbContext context)
+        {
+            _context = context;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IEnumerable<Student>> GetAllStudentAsync()
+        {
+            return await _unitOfWork.Students.GetAllAsync();
+        }
+
+        public async Task<Student> GetStudentByIdAsync(int id)
+        {
+            return await _unitOfWork.Students.GetByIdAsync(id);
+        }
+
+        public async Task AddStudentAsync(Student student)
+        {
+            await _unitOfWork.Students.AddAsync(student);
+            await _unitOfWork.CompleteAsync();
+        }
+
+        public async Task UpdateStudentAsync(Student student)
+        {
+            await _unitOfWork.Students.UpdateAsync(student);
+            await _unitOfWork.CompleteAsync();
+        }
+
+        public async Task DeleteStudentAsync(int id)
+        {
+            await _unitOfWork.Students.DeleteAsync(id);
+            await _unitOfWork.CompleteAsync();
+        }
+
+        public async Task<IEnumerable<Course>> GetAllCoursesAsync()
+        {
+            return await _context.Courses.ToListAsync();
+        }
+    }
+}
