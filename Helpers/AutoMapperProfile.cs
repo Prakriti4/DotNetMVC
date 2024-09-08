4577cc7ecc1bda1351b8fe11f5f42c3d79fc7759
Prakriti4 <114340135+Prakriti4@users.noreply.github.com>
2024-09-08 13:26:33 +0545

    Automapper used for mapping

---
+++ Helpers/AutoMapperProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using ViewModelTableFormation.Models;
+using ViewModelTableFormation.ViewModel;
+
+namespace ViewModelTableFormation.Helpers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            // Corrected the usage of ReverseMap() by adding parentheses
+            CreateMap<Student, StudentDetailsModel>().ReverseMap();
+            CreateMap<Student, CreateStudentModel>().ReverseMap();
+            CreateMap<Student, EditStudentModel>().ReverseMap();
+            CreateMap<Student, DeleteStudentModel>().ReverseMap(); 
+        }
+    }
+}
